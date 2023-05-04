e16bb45bf43c7374ac52d1aa3f114c7746fc51c5
Murat Oransoy <75497491+muratoransoy@users.noreply.github.com>
2023-05-05 01:17:28 +0300

    LenaTalentMuratOransoy

---
+++ LenaTalentMuratOransoy/LenaTalent.Entities/Models/Entities/Form.cs
@@ -0,0 +1,30 @@
+﻿using LenaTalent.Entities.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LenaTalent.Entities.Models.Entities
+{
+    public class Form
+    {
+        public Form() : base()
+        {
+            CreatedAt = DateTime.Now;
+        }
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Name alanı boş bırakılamaz.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Description alanı boş bırakılamaz.")]
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public ICollection<Field> Fields { get; set; }
+        public ICollection<FormData> FormData { get; set; }
+    }
+}
+
